46fe6e637d6ba544899435c02fd74c7a534b1631
AldaUT <aldair.utel@gmail.com>
2025-01-25 17:48:30 -0700

    Cambios al repositorio

---
+++ Ejercicios_en_Clase/clase2Modulo2.py
@@ -0,0 +1,18 @@
+#Clase Bucles o Loops: For/While
+
+correct_guess = "manzana" 
+guess_count = 0  
+guess_limit = 3 #limite de intentos
+
+while guess_count < guess_limit: 
+    guess = input("Adivina la fruta: ").lower() # Admitir mayusculas y minusculas
+    guess_count += 1 #1 input es igual a 1 intento
+    if guess == correct_guess: #si se adivina en cada intento 1>3
+        print("¡Felicidades, adivinaste la fruta!")#
+        break
+    else: #por cada intento sin adivinar
+        print("Ni modo, no adivinaste.") 
+
+if guess != correct_guess: #si se acaba los intentos sin adivinar
+    print("¡Se acabaron los intentos! La respuesta correcta era:", correct_guess) ##
+    
---
+++ Ejercicios_en_Clase/clase2Modulo7.py
@@ -0,0 +1,48 @@
+#Numeros = [1,2,3,4,5]
+#verduras = ['Zanahoria','Tomate','Brocoli','Papa', 'Cebolla']
+#datos = [0, "alex", 8.7, "hola", True, False] 
+
+#print(Numeros[-1]) #imprimir elementos de una lista
+#print(verduras[-1])
+#print(datos[-1])
+
+#Numeros.append(13)  #agregar y eliminar cosas
+#print(Numeros[-1])
+#Numeros.remove(13)
+#print(Numeros[-1])
+
+
+#.append
+#.clear
+#.count
+#.copy
+#.index
+#.insert
+#.pop
+
+#for i in Numeros:   #Poder ver todos los elementos que tenemos en una lista
+#    print(i)
+
+#Tuplas
+color = ('Rojo', 'Negro', 'verde', 'rosa', 'blanco')
+numeros = (1, 2, 3, 4, 1)
+
+#Revisa elementos de la lista
+#print(len(color))
+#print(len(numeros))
+
+#Contar elementos repetidos
+#print(numeros.count(1))
+
+#Diccionarios
+d1 = dict([
+    ('Aldair','Tepic'),
+    ('Carlos','Bogota'),
+    ('Sergio','Guanajuato'),
+    ('Josue','Guadalajara'),
+    ('Elker','Monterrey'),
+    ('Jose','Mexico'),
+]
+)
+
+print (d1)
